535b0bc624293f1ccf210782e444e303b0cb5fb1
정다해 <jdahae1225@naver.com>
2022-07-02 22:52:43 +0900

    sol num4796

--- src/num9237.java
+++ src/num9237.java
@@ -2,22 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Main {
-    /**
-     * 마지막 나무가 다 자란 뒤 이장님 초대
-     * 나무 묘목 n개 구입
-     * 하나 심는데 1일 걸림
-     * 이장님을 최대한 빨리 초대
-     * 
-     * 제일 오래 걸리는 거부터 심어
-     * 제일 오래 걸리는 거 심는데 1일 + 4일동안 자람 [] [] [] [] []
-     * [0][1][2][3][4]
-     *    [0][1][2][3]
-     *       [0][1][2][3]
-     *          [0][1][2]
-     * 심는날 + 자라는 시간 중 max + 2가 답이다.
-     *
-     */
+public class num9237 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -37,7 +22,5 @@ public class Main {
             }
         }
         System.out.println(sum+2);
-        sc.close();
     }
 }
-
