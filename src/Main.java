43cd6b54133f8d3c3f0f261c886d83491a78fdbc
정다해 <jdahae1225@naver.com>
2022-10-22 11:58:59 +0900

    backjoon_ step_greedy_1

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,31 @@
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-    /**
-     * 마지막 나무가 다 자란 뒤 이장님 초대
-     * 나무 묘목 n개 구입
-     * 하나 심는데 1일 걸림
-     * 이장님을 최대한 빨리 초대
-     * 
-     * 제일 오래 걸리는 거부터 심어
-     * 제일 오래 걸리는 거 심는데 1일 + 4일동안 자람 [] [] [] [] []
-     * [0][1][2][3][4]
-     *    [0][1][2][3]
-     *       [0][1][2][3]
-     *          [0][1][2]
-     * 심는날 + 자라는 시간 중 max + 2가 답이다.
-     *
-     */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        Integer[] tree = new Integer[N];
-        int sum = 1;
+        Scanner in = new Scanner(System.in);
 
-        for (int i = 0; i < N; i++) {
-            tree[i] = sc.nextInt();
+        //입력으로 N과 K가 주어진다.
+        int N = in.nextInt();
+        int K = in.nextInt();
+
+        int[] arr = new int[N];
+
+        //오름차순으로 주어진다.
+        for(int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
         }
-        sc.close();
 
-        Arrays.sort(tree, Collections.reverseOrder());
+        //필요한 동전 개수의 최솟값을 구하라
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            if (sum < i + tree[i]) {
-                sum += (i+tree[i])-sum;
-            }
+        int count = 0;
+        int tmp = 0; //나머지
+        for(int i = N-1 ; i >= 0; i--) {//거꾸로 탐색한다.
+            int j = K;
+            count += K / arr[i];
+            K = K % arr[i];
         }
-        System.out.println(sum+2);
-        sc.close();
+        System.out.println(count);
     }
 }
 
