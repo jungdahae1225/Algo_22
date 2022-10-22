19d8fbcd483932b260ebddc60d25d8655869cc38
정다해 <jdahae1225@naver.com>
2022-10-22 12:27:03 +0900

    backjoon_ step_greedy_2

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,43 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         //입력으로 N과 K가 주어진다.
+        //최대한 많은 회의를 진행하라
         int N = in.nextInt();
-        int K = in.nextInt();
-
-        int[] arr = new int[N];
+        int[][] arr = new int[N][2];
 
         //오름차순으로 주어진다.
         for(int i = 0; i < N; i++) {
-            arr[i] = in.nextInt();
+            arr[i][0] = in.nextInt();//회의 시작시간
+            arr[i][1] = in.nextInt();//회의 끝나는시간
         }
 
-        //필요한 동전 개수의 최솟값을 구하라
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[]a, int[]b) {
+                if(a[1] == b[1])
+                    return a[0] - b[0];
+                else
+                    return a[1] - b[1];
+            }
+        });
+
         StringBuilder sb = new StringBuilder();
 
-        int count = 0;
-        int tmp = 0; //나머지
-        for(int i = N-1 ; i >= 0; i--) {//거꾸로 탐색한다.
-            int j = K;
-            count += K / arr[i];
-            K = K % arr[i];
+        int now = arr[0][1]; //현재 진행되고 있는 회의의 끝나는 시간
+        int count = 1;
+
+        for(int i = 1 ; i < arr.length ; i++) {
+            //뒷 회의 시작시간이 앞 회의 끝나는 시점보다 전에 끝난다면
+            if(arr[i][0] >= now) {
+                now = arr[i][1];
+                count++;
+            }
         }
         System.out.println(count);
     }
 }
-
